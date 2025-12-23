6c35a4cdb527761fe94a810bb8ba93e0d0ecafaf
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2025-12-23 13:42:24 -0500

    Updated to include rss and other feed elements

--- LEGACY/go.mod
+++
@@ -1,5 +0,0 @@
-module server
-
-go 1.24.3
-
-replace SONG => ./SONG
--- LEGACY/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "SONG"
-
-func main() {
-	//nil for dm will use the internal vidi mplmentation
-	s := SONG.InitServer(".", ":8080", "veni", "vidi", "vici", nil)
-	s.Serve()
-}
